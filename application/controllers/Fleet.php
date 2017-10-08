bba4db5749834a3f36811de03c4ee66cd5526b7f
maennis <mark.andrew.ennis@gmail.com>
2017-10-07 22:53:09 -0700

    Fixed comments

--- application/controllers/Fleet.php
+++ application/controllers/Fleet.php
@@ -23,14 +23,16 @@ class Fleet extends Application
         $this->render();
     }
 
+    /**
+    * Changes the view to the view for a single plane. Adds the plane's
+    * information to the available data.
+    */
     public function show($key) {
 
         $this->data['pagebody'] = 'plane';
 
-        // build the list of authors, to pass on to our view
         $source = $this->fleetModel->get($key);
-
-        // pass on the data to present, adding the author record's fields
+        //Add plane's fields to the available data
         $this->data = array_merge($this->data, (array) $source);
 
         $this->render();
